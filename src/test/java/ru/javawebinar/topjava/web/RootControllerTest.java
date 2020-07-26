ca935a4721f7e1845fa44401970640855161e3f2
alex <speakern@rambler.ru>
2020-07-26 18:03:08 +0300

    HW7: p.1 in progress

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -2,13 +2,19 @@ package ru.javawebinar.topjava.web;
 
 import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.*;
 
 class RootControllerTest extends AbstractControllerTest {
@@ -28,5 +34,37 @@ class RootControllerTest extends AbstractControllerTest {
                             }
                         }
                 ));
+    }
+    @Test
+    void testMeals() throws Exception {
+        perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals",
+                        new AssertionMatcher<List<MealTo>>() {
+                            @Override
+                            public void assertion(List<MealTo> actual) throws AssertionError {
+                                MEAL_MATCHER.assertMatch(//actual,
+                                        new Iterable<MealTo>() {
+                                            @Override
+                                            public Iterator<MealTo> iterator() {
+                                                return null;
+                                            }
+                                        }),
+                                        //MealsUtil.getTos(MEALS, SecurityUtil.authUserCaloriesPerDay()
+                                        new Iterable<MealTo>() {
+                                            @Override
+                                            public Iterator<MealTo> iterator() {
+                                                return null;
+                                            }
+                                        }
+                                );
+                            }
+                        }
+                ));
+
+
     }
 }
